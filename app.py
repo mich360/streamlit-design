94e93d61a1bd772dc474a8c8faf58280221b543c
mich360 <65708357+mich360@users.noreply.github.com>
2023-06-01 19:04:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ with st.container():
 with st.container():
     image_column, text_column = st.columns((1, 2))  
     with image_column:
-        st.image(Image.open("images/mail
+       # st.image(Image.open("images/mail
 
 
 
