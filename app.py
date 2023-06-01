4b273e7266a2adab42d716ea8410cba056869b17
mich360 <65708357+mich360@users.noreply.github.com>
2023-06-01 19:21:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -72,3 +72,36 @@ with st.container():
         st.image(Image.open("images/mail.jpg"))
     
     with text_column:
+        st.subheader("問い合わせフォームを追加する方法")
+        st.write(
+            """
+            Webサイトに
+            Webサイトに問い合わせフォームを追加したいです。
+            以下では、無料サービス「Form Submit」を使用して、
+            Streamlitアプリに問い合わせフォームを実装する方法を説明します。
+            """
+        )
+        st.markdown("[こちら Video...](https://youtu.be/FOULV9Xij_8)")
+
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("ご連絡はこちらから!")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/masumoto.naomichi@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
+
